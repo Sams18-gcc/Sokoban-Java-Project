21d37c05c5c3459728a1d70aaeca18d45d3a0cfc
Sams <belaribisami22@gmail.com>
2026-04-06 09:13:57 +0200

    execution de l'interface directement a partir du main

--- src/sokoban/app/Main.java
+++ src/sokoban/app/Main.java
@@ -1,6 +1,7 @@
-package sokoban.UI.app;
+package sokoban.app;
 
 import javafx.application.Application;
+import sokoban.UI.app.SokobanApp;
 
 public class Main {
     public static void main(String[] args) {
