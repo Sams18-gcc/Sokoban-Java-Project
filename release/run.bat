daf036dfcc5d657cc7721de0268b2c01379c2bb7
Amayas Ait Aider <amayasaitaider2005@outlook.com>
2026-05-01 16:02:12 +0200

    Scripts : installation automatique Java si absent et double click pour lancer

--- release/run.bat
+++ release/run.bat
@@ -1,10 +1,11 @@
 @echo off
 java -version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Java n'est pas installe.
-    echo Ouverture de la page de telechargement...
+    echo Java n'est pas installe sur votre machine.
+    echo Ouverture de la page de telechargement Java 21...
     start https://adoptium.net/temurin/releases/?version=21
     pause
     exit
 )
 java -jar "%~dp0sokoban-1.0-SNAPSHOT.jar"
+pause
